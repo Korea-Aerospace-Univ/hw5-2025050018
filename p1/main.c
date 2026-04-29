0208d364b393afe2b605e0064894c1db5b55d398
2025050018 <koreaair0916@gmail.com>
2026-04-29 16:18:22 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,26 +3,26 @@
 int main() {
     int N;
     int i, j, k;
-    int found = 0;  
+    bool found = false;  
 
     scanf("%d", &N);
 
     for(i = 1; i * 900 <= N; i++) {
-        for(j = 2; j * 750 <= N; j+=2) { 
+        for(j = 2; j * 750 <= N; j += 2) {
             for(k = 1; k * 200 <= N; k++) {
 
                 if(900*i + 750*j + 200*k == N) {
 
                     if(k < i || k < j) {
                         printf("%d %d %d\n", i, j, k);
-                        found = 1;
+                        found = true; 
                     }
                 }
             }
         }
     }
 
-    if(found == 0) {
+    if(found == false) {
         printf("none\n");
     }
 
